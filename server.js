fe4cd5ece276bcce8d966ff86f219713196cd3a4
ศิริรัฐ สังข์ขาว <dodo@Macintosh-3.local>
2024-03-15 01:25:47 +0700

    set new server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+
+app.use(helmet());
+app.use(cors());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
